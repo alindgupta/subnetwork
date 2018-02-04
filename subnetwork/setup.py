bb422b77b0d6161177e648a506acdaddb0ed3793
Alind Gupta <alindgupta@alinds-mbp.gateway.2wire.net>
2018-02-04 00:09:24 -0500

    minor change in import

--- subnetwork/setup.py
+++ subnetwork/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from Cython.Build import build_ext
 import numpy as np
 
 ext  =  [Extension( "utils", sources=["util.pyx"], include_dirs = [np.get_include()])]   
